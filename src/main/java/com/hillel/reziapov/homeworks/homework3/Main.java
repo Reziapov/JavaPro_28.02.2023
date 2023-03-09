26eaefde6deff9ff1fab9bb47ba92165533c08f5
Reziapov Artur <areziapov@gmail.com>
2023-03-09 18:57:44 +0200

    change name array

--- src/main/java/com/hillel/reziapov/homeworks/homework3/Main.java
+++ src/main/java/com/hillel/reziapov/homeworks/homework3/Main.java
@@ -14,14 +14,14 @@ public class Main {
 
         if (s.length() != t.length()) return false;
 
-        char[] char1 = s.toCharArray();
-        char[] char2 = t.toCharArray();
+        char[] array1 = s.toCharArray();
+        char[] array2 = t.toCharArray();
 
-        Arrays.sort(char1);
-        Arrays.sort(char2);
+        Arrays.sort(array1);
+        Arrays.sort(array2);
 
-        for (int i = 0; i < char1.length; i++) {
-            if (char1[i] != char2[i])
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array2[i])
                 return false;
         }
         return true;
